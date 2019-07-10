ecace7e6b9729ff5983b0f8d5874ddfcf24457f7
Simona-Bit <1918118592@qq.com>
2019-07-10 16:48:00 +0800

    提交代码

---
+++ ObscuringExample.java
@@ -0,0 +1,11 @@
+//import package_name.PackageClass;
+//import java.util.Arrays;
+
+public class ObscuringExample{
+	public static void main(String[] args){
+		//优先当成变量，其次当成类型，最后当成包名
+		//int package_name = 10;
+		
+		System.out.println(package_name.PackageClass.NAME);
+	}
+}
